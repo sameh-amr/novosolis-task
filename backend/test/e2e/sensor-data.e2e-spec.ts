37e51ab691677a9213c192c152f9f271a45bca68
sameh-amr <samehamrsw@gmail.com>
2025-08-06 22:25:07 +0300

    added a unit test to the backend

---
+++ backend/test/e2e/sensor-data.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+
+
+
+describe('SensorDataController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/sensor-data (GET) should return an array of sensor data', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/sensor-data')
+      .expect(200);
+
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0]).toHaveProperty('device_id');
+  });
+});
