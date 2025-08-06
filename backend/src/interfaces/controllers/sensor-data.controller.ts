038553c2e6fc9e5a43239a4c0cd31fa099ab875e
sameh-amr <samehamrsw@gmail.com>
2025-08-06 21:55:40 +0300

    added the interface layer which represent the dtos and the controller

---
+++ backend/src/interfaces/controllers/sensor-data.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Delete, Put, Param, Body } from '@nestjs/common';
+import { FileSensorDataRepository } from '../../infrastructure/repositories/file-sensor-data.repository';
+import { GetAllSensorData } from 'src/application/use-cases/get-all-sensor-data.use-case';
+import { CreateSensorDataDto } from '../dtos/create-sensor-data.dto';
+@Controller('sensor-data')
+export class SensorDataController {
+  private repo = new FileSensorDataRepository();
+
+  @Get()
+  async getAll() {
+    const usecase = new GetAllSensorData(this.repo);
+    return usecase.execute();
+  }
+
+  @Post()
+  async create(@Body() dto: CreateSensorDataDto) {
+    const id = Date.now(); // simple ID
+    const data = { id, ...dto };
+    return this.repo.create(data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.repo.delete(Number(id));
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: Partial<CreateSensorDataDto>) {
+    return this.repo.update(Number(id), dto);
+  }
+}
---
+++ backend/src/interfaces/dtos/create-sensor-data.dto.ts
@@ -0,0 +1,6 @@
+export class CreateSensorDataDto {
+  device_id: string;
+  timestamp: string;
+  temperature: number;
+  humidity: number;
+}
