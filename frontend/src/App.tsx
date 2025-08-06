48247f12498c0bacb757624a23849a98d4904c3f
sameh-amr <samehamrsw@gmail.com>
2025-08-06 21:10:07 +0300

    added tailwind for the react project and made sure that the css is being loaded correctly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-gradient-to-br from-pink-500 to-yellow-500 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">✅ Tailwind is working!</h1>
     </div>
   );
 }
